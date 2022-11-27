6641719d77c22af604eaeabd8b742d5011828ddc
marcosajm <52014409+marcosajm@users.noreply.github.com>
2022-11-27 08:29:26 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,18 +18,18 @@ public class Main {
             }
         }
         Collections.sort(personList);
-        System.out.println("Nome do cliente no início da lista é: " + personList.get(0).getName() + "\n número de telefone: " + personList.get(0).getPhone());
+        System.out.println("Nome do cliente no início da lista para efetuar o contato: " + personList.get(0).getName() + "\n número de telefone: " + personList.get(0).getPhone());
         while (true) {
             if (personList.isEmpty()) {
                 break;
             }
             else {
                 Scanner sc = new Scanner(System.in);
-                System.out.println("Apagar cliente do início da lista emostrar o proximo? (Y)");
+                System.out.println("Apagar cliente do início da lista e mostrar o proximo da lista para efetuar o contato? (Y)");
                 if (sc.next().equalsIgnoreCase("Y")) {
                     personList.remove(0);
                     if (personList.size() >= 1) {
-                        System.out.println("Nome do cliente no início da lista é: " + personList.get(0).getName() + "\n número de telefone: " + personList.get(0).getPhone());
+                        System.out.println("Nome do cliente no início da lista para efetuar o contato: " + personList.get(0).getName() + "\n número de telefone: " + personList.get(0).getPhone());
                     }
                 }
                 else {
@@ -40,3 +40,7 @@ public class Main {
         }
     }
 }
+// programa que contemple uma fila de clientes para as chamadas de um call center.
+//        As opções do programa devem ser:
+        // Inserir cliente – Deve solicitar ao utilizador os dados Nome e Numero de telefone;
+        // Próximo cliente: – Deve ir buscar o cliente no início da lista,  removê-lo e mostrar os seus dados na consola para que o operador possa efetuar o contato com o cliente.
